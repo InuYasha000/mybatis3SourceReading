91d433a3a6014349d1cf3633953ba040f666347e
chengjj@ihdou.com <2457567745@qq.com>
2021-03-09 07:50:01 +0800

    异常模块

--- mybatis-3-master/src/main/java/org/apache/ibatis/exceptions/IbatisException.java
+++ mybatis-3-master/src/main/java/org/apache/ibatis/exceptions/IbatisException.java
@@ -18,6 +18,7 @@ package org.apache.ibatis.exceptions;
 /**
  * @author Clinton Begin
  */
+//IbatisException 已经在 2015 年被废弃，取代它的是 PersistenceException 类
 @Deprecated
 public class IbatisException extends RuntimeException {
 
